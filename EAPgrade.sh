3a611702eb7c20597d80bd08a8c2ab36aabc77b7
mamatb <amatbaeza@gmail.com>
2020-03-13 23:57:40 +0000

    Google styleguide recommendations implemented

--- EAPgrade.sh
+++ EAPgrade.sh
@@ -17,7 +17,7 @@ then
 	echo 'ERROR - root identity needed in order to deal with services' >&2
 	exit 1
 else
-	echo 'INFO - root identity confirmed, proceeding'
+	echo 'INFO - root identity confirmed, proceeding' >&2
 fi
 
 # internet access check
@@ -26,49 +26,49 @@ then
 	echo 'ERROR - internet access needed in order to clone EAPHammer' >&2
 	exit 1
 else
-	echo 'INFO - internet access confirmed, proceeding'
+	echo 'INFO - internet access confirmed, proceeding' >&2
 fi
 
 # EAPHammer git clone
-echo 'INFO - cloning EAPHammer to "'"${EAPHammer_dir}"'/"'
+echo 'INFO - cloning EAPHammer to "'"${EAPHammer_dir}"'/"' >&2
 cd '/opt/'
 git clone 'https://github.com/s0lst1c3/eaphammer.git' &> '/dev/null'
 cd "${EAPHammer_dir}/"
 
 # file movement
-echo 'INFO - moving files to "'"${EAPHammer_dir}"'/"'
+echo 'INFO - moving files to "'"${EAPHammer_dir}"'/"' >&2
 cp --force "${EAPgrade_dir}/eaphammer.sh" "${EAPgrade_dir}/eaphammer.service" "${EAPHammer_dir}/"
 
 # EAPHammer installation
-echo 'INFO - updating, installing dependencies and generating DH parameters. This is going to take a while, you can check the progress with "tail -f /tmp/EAPgrade.log" if you wish :)'
+echo 'INFO - updating, installing dependencies and generating DH parameters. This is going to take a while, you can check the progress with "tail -f /tmp/EAPgrade.log" if you wish :)' >&2
 echo -e 'y\ny' | ./kali-setup &> '/tmp/EAPgrade.log'
 
 # eaphammer.service setup
-echo 'INFO - disabling wpa_supplicant.service and dnsmasq.service so that they don'"'"'t interfere with eaphammer.service'
+echo 'INFO - disabling wpa_supplicant.service and dnsmasq.service so that they don'"'"'t interfere with eaphammer.service' >&2
 systemctl disable 'dnsmasq.service' &> '/dev/null'
 systemctl disable 'wpa_supplicant.service' &> '/dev/null'
 cp --force '/etc/dhcpcd.conf' '/etc/dhcpcd.conf.backup'
 echo 'nohook wpa_supplicant' >> '/etc/dhcpcd.conf'
 
-echo 'INFO - enabling eaphammer.service and ssh.service to administrate your Raspbian without needing to plug in keyboard and display'
+echo 'INFO - enabling eaphammer.service and ssh.service to administrate your Raspbian without needing to plug in keyboard and display' >&2
 cp --force "${EAPHammer_dir}/eaphammer.service" '/lib/systemd/system/eaphammer.service'
 systemctl enable 'ssh.service' &> '/dev/null'
 systemctl enable 'eaphammer.service' &> '/dev/null'
 
 # EAPHammer users configuration
-echo 'INFO - setting up user "EAPgrade" to access the fake AP network with password "changeme"'
+echo 'INFO - setting up user "EAPgrade" to access the fake AP network with password "changeme"' >&2
 cp --force "${EAPHammer_dir}/db/phase2.accounts" "${EAPHammer_dir}/db/phase2.accounts.backup"
 echo -e '"EAPgrade"\tGTC\t"changeme"\t[2]' > "${EAPHammer_dir}/db/phase2.accounts"
 
 # EAPHammer certs generation
-echo 'INFO - generating fake TLS certificate to use with EAPHammer'
+echo 'INFO - generating fake TLS certificate to use with EAPHammer' >&2
 ./eaphammer --bootstrap --cn 'hotspot.eapgrade.org' --country 'ES' --state 'Madrid' --locale 'Madrid' --org 'eapgrade' --org-unit 'IT' --email 'administrator@eapgrade.org' &> '/dev/null'
 
 # final steps
 chmod +x "${EAPHammer_dir}/eaphammer.sh"
-echo 'INFO - done! The WPA/WPA2-MGT fake AP attack should launch automatically after booting now, raw logs will be located at "'"${EAPHammer_dir}"'/logs/hostapd-eaphammer.raw". Next steps:'
-echo '       Modify user and password to access the fake AP network at "'"${EAPHammer_dir}"'/db/phase2.accounts"'
-echo '       Modify the ESSID at "'"${EAPHammer_dir}"'/eaphammer.sh" so that it matches your target network'
-echo '       Delete "'"${EAPHammer_dir}"'/certs/server/*", "'"${EAPHammer_dir}"'/certs/ca/*" and "'"${EAPHammer_dir}"'/certs/active/*"; and generate your own targeted certs by "'"${EAPHammer_dir}"'/eaphammer --cert-wizard"'
+echo 'INFO - done! The WPA/WPA2-MGT fake AP attack should launch automatically after booting now, raw logs will be located at "'"${EAPHammer_dir}"'/logs/hostapd-eaphammer.raw". Next steps:' >&2
+echo '       Modify user and password to access the fake AP network at "'"${EAPHammer_dir}"'/db/phase2.accounts"' >&2
+echo '       Modify the ESSID at "'"${EAPHammer_dir}"'/eaphammer.sh" so that it matches your target network' >&2
+echo '       Delete "'"${EAPHammer_dir}"'/certs/server/*", "'"${EAPHammer_dir}"'/certs/ca/*" and "'"${EAPHammer_dir}"'/certs/active/*"; and generate your own targeted certs by "'"${EAPHammer_dir}"'/eaphammer --cert-wizard"' >&2
 
 exit 0
