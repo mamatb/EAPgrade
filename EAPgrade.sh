910195ec5ba4571e606e375b2d3fa1a95bbe5831
mamatb <amatbaeza@gmail.com>
2020-03-14 01:45:23 +0000

    Google styleguide recommendations implemented

--- EAPgrade.sh
+++ EAPgrade.sh
@@ -8,8 +8,8 @@
 #
 # use colored output
 
-EAPgrade_dir="$(realpath "$(dirname "${0}")")"
-EAPHammer_dir='/opt/eaphammer'
+readonly EAPGRADE_DIR="$(realpath "$(dirname "${0}")")"
+readonly EAPHAMMER_DIR='/opt/eaphammer'
 
 # permissions check
 if [ "${USER}" != 'root' ]
@@ -30,14 +30,14 @@ else
 fi
 
 # EAPHammer git clone
-echo 'INFO - cloning EAPHammer to "'"${EAPHammer_dir}"'/"' >&2
+echo 'INFO - cloning EAPHammer to "'"${EAPHAMMER_DIR}"'/"' >&2
 cd '/opt/'
 git clone 'https://github.com/s0lst1c3/eaphammer.git' &> '/dev/null'
-cd "${EAPHammer_dir}/"
+cd "${EAPHAMMER_DIR}/"
 
 # file movement
-echo 'INFO - moving files to "'"${EAPHammer_dir}"'/"' >&2
-cp --force "${EAPgrade_dir}/eaphammer.sh" "${EAPgrade_dir}/eaphammer.service" "${EAPHammer_dir}/"
+echo 'INFO - moving files to "'"${EAPHAMMER_DIR}"'/"' >&2
+cp --force "${EAPGRADE_DIR}/eaphammer.sh" "${EAPGRADE_DIR}/eaphammer.service" "${EAPHAMMER_DIR}/"
 
 # EAPHammer installation
 echo 'INFO - updating, installing dependencies and generating DH parameters. This is going to take a while, you can check the progress with "tail -f /tmp/EAPgrade.log" if you wish :)' >&2
@@ -51,24 +51,24 @@ cp --force '/etc/dhcpcd.conf' '/etc/dhcpcd.conf.backup'
 echo 'nohook wpa_supplicant' >> '/etc/dhcpcd.conf'
 
 echo 'INFO - enabling eaphammer.service and ssh.service to administrate your Raspbian without needing to plug in keyboard and display' >&2
-cp --force "${EAPHammer_dir}/eaphammer.service" '/lib/systemd/system/eaphammer.service'
+cp --force "${EAPHAMMER_DIR}/eaphammer.service" '/lib/systemd/system/eaphammer.service'
 systemctl enable 'ssh.service' &> '/dev/null'
 systemctl enable 'eaphammer.service' &> '/dev/null'
 
 # EAPHammer users configuration
 echo 'INFO - setting up user "EAPgrade" to access the fake AP network with password "changeme"' >&2
-cp --force "${EAPHammer_dir}/db/phase2.accounts" "${EAPHammer_dir}/db/phase2.accounts.backup"
-echo -e '"EAPgrade"\tGTC\t"changeme"\t[2]' > "${EAPHammer_dir}/db/phase2.accounts"
+cp --force "${EAPHAMMER_DIR}/db/phase2.accounts" "${EAPHAMMER_DIR}/db/phase2.accounts.backup"
+echo -e '"EAPgrade"\tGTC\t"changeme"\t[2]' > "${EAPHAMMER_DIR}/db/phase2.accounts"
 
 # EAPHammer certs generation
 echo 'INFO - generating fake TLS certificate to use with EAPHammer' >&2
 ./eaphammer --bootstrap --cn 'hotspot.eapgrade.org' --country 'ES' --state 'Madrid' --locale 'Madrid' --org 'eapgrade' --org-unit 'IT' --email 'administrator@eapgrade.org' &> '/dev/null'
 
 # final steps
-chmod +x "${EAPHammer_dir}/eaphammer.sh"
-echo 'INFO - done! The WPA/WPA2-MGT fake AP attack should launch automatically after booting now, raw logs will be located at "'"${EAPHammer_dir}"'/logs/hostapd-eaphammer.raw". Next steps:' >&2
-echo '       Modify user and password to access the fake AP network at "'"${EAPHammer_dir}"'/db/phase2.accounts"' >&2
-echo '       Modify the ESSID at "'"${EAPHammer_dir}"'/eaphammer.sh" so that it matches your target network' >&2
-echo '       Delete "'"${EAPHammer_dir}"'/certs/server/*", "'"${EAPHammer_dir}"'/certs/ca/*" and "'"${EAPHammer_dir}"'/certs/active/*"; and generate your own targeted certs by "'"${EAPHammer_dir}"'/eaphammer --cert-wizard"' >&2
+chmod +x "${EAPHAMMER_DIR}/eaphammer.sh"
+echo 'INFO - done! The WPA/WPA2-MGT fake AP attack should launch automatically after booting now, raw logs will be located at "'"${EAPHAMMER_DIR}"'/logs/hostapd-eaphammer.raw". Next steps:' >&2
+echo '       Modify user and password to access the fake AP network at "'"${EAPHAMMER_DIR}"'/db/phase2.accounts"' >&2
+echo '       Modify the ESSID at "'"${EAPHAMMER_DIR}"'/eaphammer.sh" so that it matches your target network' >&2
+echo '       Delete "'"${EAPHAMMER_DIR}"'/certs/server/*", "'"${EAPHAMMER_DIR}"'/certs/ca/*" and "'"${EAPHAMMER_DIR}"'/certs/active/*"; and generate your own targeted certs by "'"${EAPHAMMER_DIR}"'/eaphammer --cert-wizard"' >&2
 
 exit 0
