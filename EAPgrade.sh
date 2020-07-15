12b17ebbfdf2fe9a7c052f50d968d614a829271a
mamatb <42239435+mamatb@users.noreply.github.com>
2020-07-15 18:46:20 +0200

    EAPgrade.sh updated with watchdog service

--- EAPgrade.sh
+++ EAPgrade.sh
@@ -37,23 +37,29 @@ cd "${EAPHAMMER_DIR}/"
 
 # file movement
 echo 'INFO - moving files to "'"${EAPHAMMER_DIR}"'/"' >&2
-cp --force "${EAPGRADE_DIR}/eaphammer.sh" "${EAPGRADE_DIR}/eaphammer.service" "${EAPHAMMER_DIR}/"
+cp --force "${EAPGRADE_DIR}/eaphammer.sh" "${EAPGRADE_DIR}/eaphammer.service" "${EAPGRADE_DIR}/eaphammer_watchdog.sh" "${EAPGRADE_DIR}/eaphammer_watchdog.service" "${EAPHAMMER_DIR}/"
 
 # EAPHammer installation
 echo 'INFO - updating, installing dependencies and generating DH parameters. This is going to take a while, you can check the progress with "tail -f /tmp/EAPgrade.log" if you wish :)' >&2
 echo -e 'y\ny' | ./kali-setup &> '/tmp/EAPgrade.log'
 
-# eaphammer.service setup
-echo 'INFO - disabling wpa_supplicant.service and dnsmasq.service so that they don'"'"'t interfere with eaphammer.service' >&2
+# services setup
+echo 'INFO - disabling wpa_supplicant.service and dnsmasq.service so that they don'"'"'t interfere' >&2
 systemctl disable 'dnsmasq.service' &> '/dev/null'
 systemctl disable 'wpa_supplicant.service' &> '/dev/null'
 cp --force '/etc/dhcpcd.conf' '/etc/dhcpcd.conf.backup'
 echo 'nohook wpa_supplicant' >> '/etc/dhcpcd.conf'
 
-echo 'INFO - enabling eaphammer.service and ssh.service to administrate your Raspbian without needing to plug in keyboard and display' >&2
+echo 'INFO - enabling eaphammer.service so that EAPHammer launches automatically after booting' >&2
 cp --force "${EAPHAMMER_DIR}/eaphammer.service" '/lib/systemd/system/eaphammer.service'
-systemctl enable 'ssh.service' &> '/dev/null'
 systemctl enable 'eaphammer.service' &> '/dev/null'
+
+echo 'INFO - enabling eaphammer_watchdog.service in order to restart EAPHammer when unstable' >&2
+cp --force "${EAPHAMMER_DIR}/eaphammer_watchdog.service" '/lib/systemd/system/eaphammer_watchdog.service'
+systemctl enable 'eaphammer_watchdog.service' &> '/dev/null'
+
+echo 'INFO - enabling ssh.service in order to administrate your Raspbian without needing to plug in keyboard and display' >&2
+systemctl enable 'ssh.service' &> '/dev/null'
 
 # EAPHammer users configuration
 echo 'INFO - setting up user "EAPgrade" to access the fake AP network with password "changeme"' >&2
@@ -66,7 +72,8 @@ echo 'INFO - generating fake TLS certificate to use with EAPHammer' >&2
 
 # final steps
 chmod +x "${EAPHAMMER_DIR}/eaphammer.sh"
-echo 'INFO - done! The WPA/WPA2-MGT fake AP attack should launch automatically after booting now, raw logs will be located at "'"${EAPHAMMER_DIR}"'/logs/hostapd-eaphammer.raw". Next steps:' >&2
+chmod +x "${EAPHAMMER_DIR}/eaphammer_watchdog.sh"
+echo 'INFO - done! Now the WPA/WPA2-MGT fake AP attack should launch automatically after booting, raw logs will be located at "'"${EAPHAMMER_DIR}"'/logs/hostapd-eaphammer.raw". Next steps:' >&2
 echo '       Modify user and password to access the fake AP network at "'"${EAPHAMMER_DIR}"'/db/phase2.accounts"' >&2
 echo '       Modify the ESSID at "'"${EAPHAMMER_DIR}"'/eaphammer.sh" so that it matches your target network' >&2
 echo '       Delete "'"${EAPHAMMER_DIR}"'/certs/server/*", "'"${EAPHAMMER_DIR}"'/certs/ca/*" and "'"${EAPHAMMER_DIR}"'/certs/active/*"; and generate your own targeted certs by "'"${EAPHAMMER_DIR}"'/eaphammer --cert-wizard"' >&2
