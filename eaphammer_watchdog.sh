d02e0029ba065d6599395f18e5a69c8b5cc86b8e
mamatb <42239435+mamatb@users.noreply.github.com>
2020-11-10 14:02:31 +0100

    s/tabs/spaces/

--- eaphammer_watchdog.sh
+++ eaphammer_watchdog.sh
@@ -7,13 +7,13 @@ readonly SERVICE='eaphammer.service'
 
 while true
 do
-	if grep --quiet --extended-regexp "(${ERROR_1}|${ERROR_2})" "${LOGFILE}"
-	then
-		if systemctl --quiet 'is-active' "${SERVICE}"
-		then
-			systemctl 'restart' "${SERVICE}"
-		fi
-		sed --regexp-extended --in-place "/(${ERROR_1}|${ERROR_2})/d" "${LOGFILE}"
-	fi
-	sleep '8'
+    if grep --quiet --extended-regexp "(${ERROR_1}|${ERROR_2})" "${LOGFILE}"
+    then
+        if systemctl --quiet 'is-active' "${SERVICE}"
+        then
+            systemctl 'restart' "${SERVICE}"
+        fi
+        sed --regexp-extended --in-place "/(${ERROR_1}|${ERROR_2})/d" "${LOGFILE}"
+    fi
+    sleep '8'
 done
