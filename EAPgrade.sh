b064ad6fb00aa88528e551ba087ce07a862ef8f7
mamatb <42239435+mamatb@users.noreply.github.com>
2021-04-13 14:35:22 +0200

    s/kali/raspbian/

--- EAPgrade.sh
+++ EAPgrade.sh
@@ -42,7 +42,7 @@ cp --force "${EAPGRADE_DIR}/eaphammer.sh" "${EAPGRADE_DIR}/eaphammer.service" "$
 
 # EAPHammer installation
 echo 'INFO - updating, installing dependencies and generating DH parameters. This is going to take a while, you can check the progress with "tail -f /tmp/EAPgrade.log" if you wish :)' >&2
-echo -e 'y\ny' | ./kali-setup &> '/tmp/EAPgrade.log'
+echo -e 'y\ny' | ./raspbian-setup &> '/tmp/EAPgrade.log'
 
 # services setup
 echo 'INFO - disabling wpa_supplicant.service and dnsmasq.service so that they don'"'"'t interfere' >&2
