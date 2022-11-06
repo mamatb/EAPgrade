df6864a48208f93439e84d959ada7eb5204c8515
mamatb <42239435+mamatb@users.noreply.github.com>
2022-11-06 23:14:54 +0100

    use of ehdb instead of manually modifying the files

--- EAPgrade.sh
+++ EAPgrade.sh
@@ -72,8 +72,8 @@ systemctl --quiet enable 'ssh.service'
 
 # EAPHammer users configuration
 echo 'INFO - setting up user "EAPgrade" to access the fake AP network with password "changeme"' >&2
-cp --force "${EAPHAMMER_DIR}/db/phase2.accounts" "${EAPHAMMER_DIR}/db/phase2.accounts.backup"
-echo -e '"EAPgrade"\tGTC\t"changeme"\t[2]' > "${EAPHAMMER_DIR}/db/phase2.accounts"
+./ehdb --delete --delete-all
+./ehdb --add --identity 'EAPgrade' --password 'changeme'
 
 # EAPHammer certs generation
 echo 'INFO - generating fake TLS certificate to use with EAPHammer' >&2
@@ -85,6 +85,6 @@ chmod +x "${EAPHAMMER_DIR}/eaphammer_watchdog.sh"
 echo 'INFO - done! Now the WPA/WPA2-MGT fake AP attack should launch automatically after booting, raw logs will be located at "'"${EAPHAMMER_DIR}"'/logs/hostapd-eaphammer.raw". Next steps:' >&2
 echo '       Modify user and password to access the fake AP network at "'"${EAPHAMMER_DIR}"'/db/phase2.accounts"' >&2
 echo '       Modify the ESSID at "'"${EAPHAMMER_DIR}"'/eaphammer.sh" so that it matches your target network' >&2
-echo '       Delete "'"${EAPHAMMER_DIR}"'/certs/server/*", "'"${EAPHAMMER_DIR}"'/certs/ca/*" and "'"${EAPHAMMER_DIR}"'/certs/active/*"; and generate your own targeted certs by "python3 '"${EAPHAMMER_DIR}"'/eaphammer --cert-wizard" (may need to install some Python 3 modules)' >&2
+echo '       Delete "'"${EAPHAMMER_DIR}"'/certs/server/*", "'"${EAPHAMMER_DIR}"'/certs/ca/*" and "'"${EAPHAMMER_DIR}"'/certs/active/*"; and generate your own targeted certs by "python3 '"${EAPHAMMER_DIR}"'/eaphammer --cert-wizard"' >&2
 
 exit 0
