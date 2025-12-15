1eb7dc8eb77a1499a9fcdb791939c98256cbf064
mamatb <42239435+mamatb@users.noreply.github.com>
2025-12-15 15:40:42 +0100

    TODOs updated

--- EAPgrade.sh
+++ EAPgrade.sh
@@ -10,6 +10,7 @@
 #
 # update hardware section
 # s/raspbian/raspberry pi os/
+# limit the maximum line length
 # grep interface names instead of assuming 'wlan0' and 'wlan1'
 # make use of 5ghz and channel bonding
 # exclude wlan* from networkmanager instead of modifying services
